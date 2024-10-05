4e001b73cc23ba214f949518c96f6c1a84a9c99a
Jean-Ivan-Ko <ivankopytin@gmail.com>
2024-10-05 11:28:57 +0300

    Update ok2.2-6.py

--- LABS/lab 2.2/ok2.2-6.py
+++ LABS/lab 2.2/ok2.2-6.py
@@ -1,9 +1,9 @@
 import sys
 
-sys.setrecursionlimit(10 ** 6)
+sys.setrecursionlimit(10 ** 6)#Устанавливает лимит рекурсии
 
 
-def is_bst(tree, idx, min_key, max_key):
+def is_bst(tree, idx, min_key, max_key):#Проверяет, является ли текущий элемент дерева допустимым ключом. Если нет, то возвращает False
     if idx == -1:
         return True
 
@@ -12,20 +12,20 @@ def is_bst(tree, idx, min_key, max_key):
     if not (min_key < key < max_key):
         return False
 
-    return is_bst(tree, l, min_key, key) and is_bst(tree, r, key, max_key)
+    return is_bst(tree, l, min_key, key) and is_bst(tree, r, key, max_key)#Рекурсивно вызывает функцию is_bst для левого и правого поддеревьев
 
 
-with open("input.txt", "r") as f:
+with open("input.txt", "r") as f:#Считывает количество элементов бинарного дерева из файла input.txt
     n = int(f.readline().strip())
 
-    if n == 0:
+    if n == 0:#Если количество элементов равно нулю, то дерево корректно, и код выводит «CORRECT» в файл output.txt
         with open("output.txt", "w") as out:
             out.write("CORRECT")
         exit(0)
 
-    tree = [tuple(map(int, f.readline().split())) for _ in range(n)]
+    tree = [tuple(map(int, f.readline().split())) for _ in range(n)]#Создаёт бинарное дерево из данных, считанных из файла
 
-if is_bst(tree, 0, -float('inf'), float('inf')):
+if is_bst(tree, 0, -float('inf'), float('inf')):#Если функция возвращает True, то дерево является корректным, иначе — некорректным
     with open("output.txt", "w") as out:
         out.write("CORRECT")
 else:
